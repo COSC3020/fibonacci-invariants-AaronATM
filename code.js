922ea466271da5ae5c699a7c06dd3c7f5e042e15
Aaron Krapes <aaronkrapes@outlook.com>
2024-03-19 11:21:40 -0600

    updated code.js

--- code.js
+++ code.js
@@ -1,3 +1,29 @@
-function fib(n) {
-    return n;
+function fib(n) 
+{
+    array = new Array(n);
+
+    if (n >= 0)
+    {
+        array[0] = 0;
+    }
+
+    if (n >= 1)
+    {
+        array[1] = 1;
+    }
+
+    calculate(n, array)
+    return array;
+}
+
+function calculate(n, array)
+{
+    if (n < 2)
+    {
+        return;
+    }
+
+    calculate(n - 1, array);
+    array[n] = array[n - 1] + array[n - 2];
+    return;
 }
